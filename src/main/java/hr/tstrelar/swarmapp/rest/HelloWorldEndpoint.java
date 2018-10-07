1e70ba368933773f181884f17d06473ce5fd9c61
Tomislav <tstrelar@gmail.com>
2018-10-07 15:40:18 +0200

    Add logging (doesn't write to file)

--- src/main/java/hr/tstrelar/swarmapp/rest/HelloWorldEndpoint.java
+++ src/main/java/hr/tstrelar/swarmapp/rest/HelloWorldEndpoint.java
@@ -1,6 +1,8 @@
 package hr.tstrelar.swarmapp.rest;
 
 
+import org.jboss.logging.Logger;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
@@ -10,9 +12,12 @@ import javax.ws.rs.Produces;
 @Path("/hello")
 public class HelloWorldEndpoint {
 
+	private static Logger log = Logger.getLogger(HelloWorldEndpoint.class);
+
 	@GET
 	@Produces("text/plain")
 	public Response doGet() {
+		log.info("I was invoked, better say hello...");
 		return Response.ok("Hello from Thorntail!").build();
 	}
 }
