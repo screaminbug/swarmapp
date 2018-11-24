351487425337b78e5c8ade0042b527c7ba364224
Tomislav <tstrelar@gmail.com>
2018-11-24 09:45:01 +0100

    Test JSON serialization

---
+++ src/main/java/hr/tstrelar/swarmapp/rest/Data.java
@@ -0,0 +1,73 @@
+package hr.tstrelar.swarmapp.rest;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
+public class Data {
+    private final int id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ssX")
+    private final Date currentDate;
+    private final String someText;
+    private final double someDouble;
+
+    public Data(int id, Date currentDate, String someText, double someDouble) {
+        this.id = id;
+        this.currentDate = currentDate;
+        this.someText = someText;
+        this.someDouble = someDouble;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getCurrentDate() {
+        return currentDate;
+    }
+
+    public String getSomeText() {
+        return someText;
+    }
+
+    public double getSomeDouble() {
+        return someDouble;
+    }
+
+    public static class Builder {
+        private int id;
+        private Date currentDate;
+        private String someText;
+        private double someDouble;
+
+        public static Builder create() {
+            return new Builder();
+        }
+
+        public Builder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder date(Date date) {
+            this.currentDate = date;
+            return this;
+        }
+
+        public Builder text(String text) {
+            this.someText = text;
+            return this;
+        }
+
+        public Builder aDouble(double someDouble) {
+            this.someDouble = someDouble;
+            return this;
+        }
+
+        public Data build() {
+            return new Data(id, currentDate, someText, someDouble);
+        }
+
+
+    }
+}
--- src/main/java/hr/tstrelar/swarmapp/rest/HelloWorldEndpoint.java
+++ src/main/java/hr/tstrelar/swarmapp/rest/HelloWorldEndpoint.java
@@ -1,12 +1,14 @@
 package hr.tstrelar.swarmapp.rest;
 
 
+import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.jboss.logging.Logger;
 
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Date;
 
 
 @Path("/hello")
@@ -15,9 +17,20 @@ public class HelloWorldEndpoint {
 	private static Logger log = Logger.getLogger(HelloWorldEndpoint.class);
 
 	@GET
-	@Produces("text/plain")
-	public Response doGet() {
+	@Produces(MediaType.APPLICATION_JSON)
+	@Counted(
+			name="hello.counter",
+			displayName="Number of greetings",
+			description = "Greetings that were given using any method",
+			monotonic=true,
+			reusable=true)
+	public Data doGet() {
 		log.info("I was invoked, better say hello...");
-		return Response.ok("Hello from Thorntail!").build();
+		return Data.Builder.create()
+				.aDouble(5.4)
+				.date(new Date())
+				.id(43)
+				.text("This is THE text")
+				.build();
 	}
 }
